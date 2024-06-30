60b79b2352a84cc21af7399f4a7f12d7f4957eb3
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-01 05:14:24 +0800

    deleted comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Reader from "./views/Reader";
-// import { ThemeProvider } from "./context/ThemeContext";
 import AllProvider from "./provider/AllProvider";
 
 function App() {
